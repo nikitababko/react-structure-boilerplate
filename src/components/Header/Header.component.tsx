0f16a854c0115c67f88cb3d1d5f982a63fbf4fec
Nikita Babko <niki.babko@gmail.com>
2022-10-18 17:34:33 +0700

    1) Added config for eslint and prettier; 2) Added project author to 'pkg.json'

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import LogoSVG from "../../assets/Header/Logo.svg";
-import { Image } from "../UI/Image";
+import LogoSVG from '../../assets/Header/Logo.svg';
+import { Image } from '../UI/Image';
 
-import { Container } from "./Header.styles";
+import { Container } from './Header.styles';
 
 export const Header: React.FC = () => {
+  const arr = [
+    {
+      id: 1,
+    },
+  ];
+
+  if (arr.length > 1) {
+    console.log('ASD');
+  }
+
   return (
     <Container>
       <Image
